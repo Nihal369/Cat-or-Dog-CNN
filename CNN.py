43bec8d6e1d3996154a6b665be38b83d2009f0de
Nihal Ismail K <nihalismailk@gmail.com>
2017-10-18 17:25:53 +0530

    Imported libraries

--- CNN.py
+++ CNN.py
@@ -5,3 +5,13 @@ Created on Wed Oct 18 16:09:46 2017
 @author: nihal369
 """
 
+#CNN Architecture
+
+#Importing libraries
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+
+
